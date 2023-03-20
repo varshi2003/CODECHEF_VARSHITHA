74b97fa554f228fbb6deca41a817b7348e4fbc86
varshi2003 <113238130+varshi2003@users.noreply.github.com>
2023-03-20 11:54:15 +0530

    Add files via upload

---
+++ ICM0013.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+	    int a,c=0;
+	    scanf("%d",&a);
+	    for(int i=1;i<=a;i++)
+	    {
+	        
+	           c++;
+	        
+	    }
+	    printf("%d\n",c);
+	}
+}
+
+
+
