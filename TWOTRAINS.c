dc6ea0359990f62527bc9fd46d8eb59c2f4722d4
varshi2003 <113238130+varshi2003@users.noreply.github.com>
2023-04-20 09:40:17 +0530

    Add files via upload

---
+++ TWOTRAINS.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(int n){
+    for(scanf("%*d");~scanf("%d",&n);){
+        int sum=0,max=0;
+        for(int p;--n;){
+            scanf("%d",&p);
+            if(max<p)
+            max=p;
+            sum+=p;
+        }
+        printf("%d\n",sum+max);
+    }
+	return 0;
+}
